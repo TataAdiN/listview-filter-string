f6b92709004019002cdb98f0ad1b8d1b3d9d1241
tataadin <tataa2237@gmail.com>
2021-06-08 09:23:52 +0700

    show list to recyclerview

--- app/src/main/java/com/tataadin/myapplication/MainActivity.java
+++ app/src/main/java/com/tataadin/myapplication/MainActivity.java
@@ -1,13 +1,33 @@
 package com.tataadin.myapplication;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
 import androidx.appcompat.app.AppCompatActivity;
+import com.tataadin.myapplication.databinding.ActivityMainBinding;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+    private ActivityMainBinding viewBinding;
+    private ArrayAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        viewBinding = ActivityMainBinding.inflate(getLayoutInflater());
+
+        ArrayList<String> names = new ArrayList<>();
+        names.add("Mitch");
+        names.add("Blake");
+        names.add("Shelly");
+        names.add("Jess");
+        names.add("Steve");
+        names.add("Mohammed");
+
+        adapter = new ArrayAdapter(this, R.layout.list_item_layout, names);
+        viewBinding.theList.setAdapter(adapter);
+
+        setContentView(viewBinding.getRoot());
     }
+
 }
