9e7436aa81386240f3bcb5e349bf66dd09de7624
tataadin <tataa2237@gmail.com>
2021-06-08 09:45:54 +0700

    implement recyclerview filter

--- app/src/main/java/com/tataadin/myapplication/MainActivity.java
+++ app/src/main/java/com/tataadin/myapplication/MainActivity.java
@@ -1,6 +1,8 @@
 package com.tataadin.myapplication;
 
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.widget.ArrayAdapter;
 import androidx.appcompat.app.AppCompatActivity;
 import com.tataadin.myapplication.databinding.ActivityMainBinding;
@@ -9,7 +11,7 @@ import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
     private ActivityMainBinding viewBinding;
-    private ArrayAdapter adapter;
+    private ArrayAdapter<String> adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,8 +26,25 @@ public class MainActivity extends AppCompatActivity {
         names.add("Steve");
         names.add("Mohammed");
 
-        adapter = new ArrayAdapter(this, R.layout.list_item_layout, names);
+        adapter = new ArrayAdapter<>(this, R.layout.list_item_layout, names);
         viewBinding.theList.setAdapter(adapter);
+
+        viewBinding.searchFilter.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                (MainActivity.this).adapter.getFilter().filter(charSequence);
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+
+            }}
+        );
 
         setContentView(viewBinding.getRoot());
     }
